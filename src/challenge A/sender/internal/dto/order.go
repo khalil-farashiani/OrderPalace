9fbf837cbe95f20f55c0d9917f4a481cf0960e6f
Khalil Farashiani <khalilfarashi2000@gmail.com>
2023-02-23 18:25:41 +0330

    add validate method for validation order dto

--- src/challenge A/sender/internal/dto/order.go
+++ src/challenge A/sender/internal/dto/order.go
@@ -1,6 +1,10 @@
 package dto
 
-import "github.com/khalil-farashiani/OrderPalace/sender/internal/entities"
+import (
+	"errors"
+	"github.com/khalil-farashiani/OrderPalace/sender/internal/entities"
+	"strings"
+)
 
 type SendOrderRequest struct {
 	entities.Order
@@ -8,4 +12,22 @@ type SendOrderRequest struct {
 
 type SendOrderResponse struct {
 	Message string `json:"message"`
+}
+
+const (
+	requiredIdErrorMsg    = "id is required field"
+	requiredPriceErrorMsg = "price is required field and should be positive integer"
+	requiredTitleErrorMsg = "title cannot be empty"
+)
+
+func (s SendOrderRequest) Validate() error {
+	switch {
+	case s.ID == 0:
+		return errors.New(requiredIdErrorMsg)
+	case s.Price <= 0:
+		return errors.New(requiredPriceErrorMsg)
+	case strings.TrimSpace(s.Title) == "":
+		return errors.New(requiredTitleErrorMsg)
+	}
+	return nil
 }

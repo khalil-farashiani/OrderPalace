7f172259a39784db083d958e5a05c54e49dedcb5
Khalil Farashiani <khalilfarashi2000@gmail.com>
2023-02-23 18:26:09 +0330

    add redis to project as sub pub tool

---
+++ src/challenge A/sender/internal/adapters/broker/redis/add_order.go
@@ -0,0 +1,33 @@
+package redis
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/khalil-farashiani/OrderPalace/sender/internal/entities"
+)
+
+// AddOrder adds an order to the Redis queue.
+func (q RedisQueue) AddOrder(ctx context.Context, order *entities.Order) error {
+	conn := q.pool.Get()
+	defer conn.Close()
+
+	orderJSON, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to marshal order: %v", err)
+	}
+
+	//connWithContext := conn(ctx)
+	//if err := connWithContext.Err(); err != nil {
+	//	return fmt.Errorf("failed to create context with Redis connection: %v", err)
+	//}
+
+	if err := conn.Send("RPUSH", "orders", orderJSON); err != nil {
+		return fmt.Errorf("failed to send order to Redis queue: %v", err)
+	}
+
+	if err := conn.Flush(); err != nil {
+		return fmt.Errorf("failed to flush Redis connection: %v", err)
+	}
+	return nil
+}
---
+++ src/challenge A/sender/internal/adapters/broker/redis/init.go
@@ -0,0 +1,25 @@
+package redis
+
+import "github.com/gomodule/redigo/redis"
+
+// RedisQueue represents a Redis-backed queue for storing orders.
+type RedisQueue struct {
+	pool *redis.Pool
+}
+
+// NewRedisQueue creates a new RedisQueue with the given Redis connection pool.
+func NewRedisQueue(pool *redis.Pool) *RedisQueue {
+	return &RedisQueue{pool}
+}
+
+func InitRedis(dsn string) *RedisQueue {
+	redisPool := &redis.Pool{
+		MaxIdle:   5,
+		MaxActive: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", dsn, redis.DialPassword("testpass"))
+		},
+	}
+	redisQ := NewRedisQueue(redisPool)
+	return redisQ
+}

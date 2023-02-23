2a8feacaa9a70d81f2018249ac66ad091e5efe29
khalil_farashiani <khalilfarashi2000@gmail.com>
2023-02-24 00:58:13 +0330

    complete challenge B

---
+++ src/challenge B/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+// findExceptionNumber takes a slice of integers arr and
+// returns the exception number by performing a bitwise XOR operation on all the numbers in the slice.
+// The ^ operator in Go is used for bitwise XOR.
+// Notice that this solution can respond correct answer only when slice or array item repeated twice except for 1 number
+//(exactly that is question say)
+func findExceptionNumber(arr []int) int {
+	result := 0
+	for _, num := range arr {
+		result ^= num
+	}
+	return result
+}
+func main() {
+	arr := []int{2, 2, 5, 6, 5, 6, 9}
+	exceptionNumber := findExceptionNumber(arr)
+	fmt.Println(exceptionNumber) // prints 6
+}

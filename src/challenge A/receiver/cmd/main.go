86fe5d9b17b113ce8ba421d321206f016ce9d461
khalil_farashiani <khalilfarashi2000@gmail.com>
2023-02-25 00:38:52 +0330

    complete main func

--- src/challenge A/receiver/cmd/main.go
+++ src/challenge A/receiver/cmd/main.go
@@ -1,5 +1,48 @@
-package cmd
+package main
+
+import (
+	"context"
+	"github.com/khalil-farashiani/OrderPalace/receiver/config"
+	"github.com/khalil-farashiani/OrderPalace/receiver/internal/adapter/broker/redis"
+	"github.com/khalil-farashiani/OrderPalace/receiver/internal/adapter/store/mysql"
+	"github.com/khalil-farashiani/OrderPalace/receiver/internal/interactor/order"
+	"log"
+	"os"
+)
+
+const (
+	redisDSN     = "REDIS_DSN"
+	mySqlDSN     = "MYSQL_DSN"
+	portErrorMsg = "port should be pass as argument"
+)
 
 func main() {
+	appSetting := GetAppConfig()
+	redisQ := redis.InitRedis(appSetting.BrokerDSN)
+	mysqlStore := mysql.InitMySql("default_user:default_password@tcp(127.0.0.1:3306)/example_db")
+	orderInteractor := order.New(*redisQ, mysqlStore)
 
+	log.Println("start consuming orders")
+	ctx := context.Background()
+	go orderInteractor.CreateOrder(ctx)
+
+	forever := make(chan bool)
+	go func() {
+		for {
+
+		}
+	}()
+	<-forever // Block main from exiting
+}
+
+func GetAppConfig() config.AppConfig {
+	if len(os.Args) < 2 {
+		log.Fatalf(portErrorMsg)
+	}
+	return config.AppConfig{
+		InProduction: false,
+		BrokerDSN:    os.Getenv(redisDSN),
+		MySQLDSN:     os.Getenv(mySqlDSN),
+		PortServer:   os.Args[1],
+	}
 }
